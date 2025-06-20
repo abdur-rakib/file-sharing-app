7dd30935f4d37059160b35933879235a81dcd321
Abdur Rakib <abdurrakib961@gmail.com>
2025-06-20 09:24:03 +0600

    refactor(storage): reorder imports for improved readability in storage module

--- codes/backend/src/modules/storage/storage.module.ts
+++ codes/backend/src/modules/storage/storage.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { StorageService } from "./storage.service";
-import { StorageFactory } from "./storage.factory";
-import { LocalStorageProvider } from "./providers/local-storage.provider";
 import { GoogleStorageProvider } from "./providers/google-storage.provider";
+import { LocalStorageProvider } from "./providers/local-storage.provider";
+import { StorageFactory } from "./storage.factory";
+import { StorageService } from "./storage.service";
 
 @Module({
   providers: [
